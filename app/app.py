162a54dcb49d059f1a3fd9708dd4af393ff351ff
Tim Enders <enders.tim@gmail.com>
2017-05-01 22:01:33 -0500

    added templates for rules uploads and finished upload func

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask_uploads import UploadSet, configure_uploads
 app = Flask(__name__)
 
 rules = UploadSet('rules', extensions='xml')
-app.config['UPLOADED_RULES_DEST'] = 'static/rules/'
+app.config['UPLOADED_RULES_DEST'] = '../rules'
 configure_uploads(app, rules)
 
 @app.route('/')
@@ -23,3 +23,19 @@ def api_checker():
         return render_template('api_results.html', results="API Key is good!")
     else:
         return render_template('api_results.html', results="API Key is bad, please generate a new key.")
+
+@app.route('/rules', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'rules' in request.files:
+        filename = photos.save(request.files['rules'])
+        rec = Photo(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Rule template saved.")
+        return redirect(url_for('show_rules', id=rec.id))
+    return render_template('rules.html')
+
+@app.route('/show_rules')
+def show_rules():
+    return render_template(
+        'show_rules.html',
+    )
