8fc72baa0453485a3558ae9d445ed4ed8da7ab1b
Tim Enders <enders.tim@gmail.com>
2017-05-02 16:25:26 -0500

    reconfigured to properly upload files

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_uploads import UploadSet, configure_uploads
 
 app = Flask(__name__)
+app.secret_key = 'asdfkjqwg[q89ei4yut;oqeirhg[03w4higr'
 
 rules = UploadSet('rules', extensions='xml')
-app.config['UPLOADED_RULES_DEST'] = '../rules'
+app.config['UPLOADED_RULES_DEST'] = 'static/rules'
 configure_uploads(app, rules)
 
 @app.route('/')
@@ -27,11 +28,9 @@ def api_checker():
 @app.route('/rules', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'rules' in request.files:
-        filename = photos.save(request.files['rules'])
-        rec = Photo(filename=filename, user=g.user.id)
-        rec.store()
+        filename = rules.save(request.files['rules'])
         flash("Rule template saved.")
-        return redirect(url_for('show_rules', id=rec.id))
+        return redirect(url_for('show_rules'))
     return render_template('rules.html')
 
 @app.route('/show_rules')
