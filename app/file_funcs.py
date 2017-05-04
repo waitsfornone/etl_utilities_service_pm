19c779040c4528c420857c15f562d3adb3c53eed
Tim Enders <enders.tim@gmail.com>
2017-05-04 14:05:49 -0500

    loop and provide links to files

--- app/file_funcs.py
+++ app/file_funcs.py
@@ -5,6 +5,6 @@ def list_files(root_path, path):
     filepath_list = []
     uploads = os.path.join(root_path, path)
     for filename in os.listdir(uploads):
-        filepath = uploads + '/' + filename
+        filepath = path + '/' + filename
         filepath_list.append(filepath)
     return filepath_list
