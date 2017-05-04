2f988c2e0eed68577a4238abb8e3e87eb2578faa
Tim Enders <enders.tim@gmail.com>
2017-05-04 13:50:45 -0500

    properly providing absolute paths for listing files for download

--- app/file_funcs.py
+++ app/file_funcs.py
@@ -2,6 +2,9 @@ import os
 
 
 def list_files(root_path, path):
+    filepath_list = []
     uploads = os.path.join(root_path, path)
-    print(os.listdir(uploads))
-    return os.listdir(uploads)
+    for filename in os.listdir(uploads):
+        filepath = uploads + '/' + filename
+        filepath_list.append(filepath)
+    return filepath_list
