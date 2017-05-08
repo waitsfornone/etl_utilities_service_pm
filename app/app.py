f1a83e3965285911ee8e2e8653a0a3977275e012
Tim Enders <enders.tim@gmail.com>
2017-05-08 13:08:57 -0500

    Added Functional Specs storage

--- app/app.py
+++ app/app.py
@@ -15,7 +15,9 @@ app = Flask(__name__)
 app.secret_key = 'asdfkjqwg[q89ei4yut;oqeirhg[03w4higr'
 
 rules = UploadSet('rules', extensions='xml')
+funcspec = UploadSet('funcspec', extensions='pdf')
 app.config['UPLOADED_RULES_DEST'] = 'static/rules'
+app.config['UPLOADED_FUNCSPEC_DEST'] = 'static/func_specs'
 configure_uploads(app, rules)
 
 
@@ -66,6 +68,34 @@ def serve_rules(path):
     filename = path.split('/')[-1]
     return send_from_directory(
         app.config['UPLOADED_RULES_DEST'],
+        filename,
+        as_attachment=True
+    )
+
+
+@app.route('/func_specs', methods=['GET', 'POST'])
+def upload_func_specs():
+    if request.method == 'POST' and 'funcspec' in request.files:
+        funcspec.save(request.files['funcspec'])
+        flash("Functional Specifications saved.")
+        return redirect(url_for('show_func_specs'))
+    return render_template('func_specs.html')
+
+
+@app.route('/show_func_specs')
+def show_func_specs():
+    files = list_files(app.root_path, app.config['UPLOADED_FUNCSPEC_DEST'])
+    return render_template(
+        'show_func_specs.html',
+        files=files
+    )
+
+
+@app.route('/<path:path>', methods=['GET'])
+def serve_func_specs(path):
+    filename = path.split('/')[-1]
+    return send_from_directory(
+        app.config['UPLOADED_FUNCSPEC_DEST'],
         filename,
         as_attachment=True
     )
--- app/file_funcs.py
+++ app/file_funcs.py
@@ -6,4 +6,4 @@ def list_files(root_path, path):
     uploads = os.path.join(root_path, path)
     for filename in os.listdir(uploads):
         filepath_list.append(filename)
-    return filepath_list
+    return filepath_list.sort()
